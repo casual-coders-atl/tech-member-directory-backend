ec45afd766ab05e002f4cb61e0a86a3c4dcac3e5
Ben Bynum <ben@bbynum.com>
2023-03-03 21:55:54 -0500

    Initialize project: OpenJdk 19.0.2, Spring 3.0.4, Gradle 7.6.1; Update .gitignore

---
+++ src/main/java/com/casualcodersatl/tmd/TechMemberDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.casualcodersatl.tmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMemberDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechMemberDirectoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/casualcodersatl/tmd/TechMemberDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casualcodersatl.tmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMemberDirectoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
